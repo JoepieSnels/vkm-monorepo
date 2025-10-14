b5a7e8fb6e733bb66e605af6a23efe63126ef3f9
JoepieSnels <j.snels@student.avans.nl>
2025-10-14 11:35:30 +0200

    homepage zou nu er goed uit moeten zien en onnodige files zijn weg

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -10,12 +10,12 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/create-module',
+      name: 'create-module',
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (CreateModule.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/CreateModule.vue'),
     },
   ],
 })
